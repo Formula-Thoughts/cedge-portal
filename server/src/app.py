932c6f5b72907684777bbb5510941283d4821ce8
roboc <aidanwilliamgannon@gmail.com>
2023-10-29 23:34:24 +0000

    add chatgpt invoking logic

--- server/src/app.py
+++ server/src/app.py
@@ -1,5 +1,49 @@
+import json
+import os
+
+import openai
+
+
 def lambda_handler(event, context):
-    return {
-        "statusCode": 200,
-        "body": "stuff"
+    default_headers = {
+        'Content-Type': 'application/json'
     }
+    try:
+        job_listing_json = json.loads(event['body'])
+
+        openai.api_key = os.getenv("OPENAI_API_KEY")
+
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {
+                    "role": "system",
+                    "content": "You are a helpful assistant, that processes html-based content job listings "
+                               "for potential job applications writing their CV"
+                },
+                {
+                    "role": "assistant", "name": "example_response_format_schema",
+                    "content": "{\"summaries\": [\"SUMMARY 1\", \"SUMMARY 2\", \"SUMMARY 3\"]}"
+                },
+                {
+                    "role": "user",
+                    "content": f"take this job listing: \"{job_listing_json}\" and write 3 summaries following the json "
+                               f"schema provided, and only return the json"
+                }
+            ],
+            temperature=0.1
+        )
+
+        return {
+            'statusCode': 200,
+            'body': json.dumps(response['choices'][0]['message']['content']),
+            'headers': default_headers
+        }
+    except Exception as e:
+        print(f"error occurred {e}")
+
+        return {
+            'statusCode': 500,
+            'body': '{\'message\': \'internal server error :(\'}',
+            'headers': default_headers
+        }
